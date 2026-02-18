2b3cb1cae4566e2e7dc3728bb3f67c25ab0eeed0
alexander <4lexander1234@gmail.com>
2026-02-18 03:09:02 -0500

    actualizando

--- Backend/config/app.js
+++ Backend/config/app.js
@@ -18,7 +18,7 @@ function createApp() {
     app.use(express.static(path.join(__dirname, "../../Frontend/index")));
 
     // Fallback para SPA
-    app.get("/" ,(req, res) => {
+    app.get(".*" ,(req, res) => {
     res.sendFile(path.join(__dirname, "../../Frontend/index/index.html"));
     });
 

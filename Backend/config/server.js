53c23a6673ee9410ae67b03f2d7a6f1d9471fc61
alexander <4lexander1234@gmail.com>
2026-02-18 02:44:44 -0500

    actualizando PORT

--- Backend/config/server.js
+++ Backend/config/server.js
@@ -5,14 +5,19 @@ const app = createApp();
 
 // probar la conexion a la base de datos y lugo iniciar el servidor
 (async () => {
-  const isConnected = await checkConnection();
-  if (!isConnected) {
-    console.error("No se pudo conectar a Supabase. Cerrando servidor...");
+  try {
+    const isConnected = await checkConnection();
+    if (!isConnected) {
+      console.error("No se pudo conectar a Supabase. Cerrando servidor...");
+      process.exit(1);
+    }
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error al iniciar el servidor:", err);
     process.exit(1);
   }
-
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-  });
 })();
